e98191de350c00eaa490061aa80f03c5b60661c3
Denise Pendleton <dpendl1@hotmail.com>
2018-04-19 22:09:30 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_04_19_183614) do
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
-    t.time "cook_time"
+    t.string "cook_time"
   end
 
 end
