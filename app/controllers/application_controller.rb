6c920f34efe50d7b3ad5aeb848ea472e2ff75d83
Denise Pendleton <dpendl1@hotmail.com>
2018-04-19 22:25:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/:id' do
     # binding.pry
-    @recipe.id = params[:id]
+    # @recipe.id = params[:id]
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.id = params[:id]
     erb :show
