e58c8c437745643bc35d89d6327ab11b8f2c1118
Denise Pendleton <dpendl1@hotmail.com>
2018-04-20 01:12:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  
+
 
   post '/recipes' do
     @recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
